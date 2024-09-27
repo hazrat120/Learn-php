2b0a9ac10c33024e048f61d85b4affa5235d8756
hazrat120 <125098771+hazrat120@users.noreply.github.com>
2024-09-27 12:58:09 +0600

    if elseif practice

---
+++ index.php
@@ -0,0 +1,183 @@
+
+<!-- php practice -->
+<?php 
+    // $x = 15;
+    // define('A', 15);
+    // echo (A);
+    // var_dump(A);
+
+    // define('cars', ['BMW', 'Toyota', 'Volvo', 'Marcitis', 'Mazda']);
+    // echo(cars[0]);
+    // echo(cars[1]);
+    // echo(cars[2]);
+    // echo(cars[3]);
+    // var_dump(cars);
+
+    // function name(){
+    //     $x = 10;
+    //     echo($x);
+    //     echo(cars[3]);
+    // };
+    // name();
+
+    // function name1(){
+    //     $B = 'Bold';
+    //     echo ($B);
+    //     echo(cars[1]);
+    // };
+    // name1();
+
+?>
+
+
+<?php 
+    // $X = 10;
+    // $Y = 20;
+    // $Z = 15;
+    // $A = $X + $Y;
+    // $B = '10';
+
+    // echo($X + $Y + $Z . '<br>');
+    // echo $X % $Y . "<br>";
+    // echo $Z ** $X . '<br>';
+    // echo $A;
+
+    // echo ( $X == $B);
+    // var_dump($X == $B);
+
+    // class Car{
+    //     public $name;
+    //     public $color;
+
+    //     function __construct($name, $color){
+    //         $this->name = $name;
+    //         $this->color = $color;
+    //     } 
+
+    //     function getInfo(){
+    //         return '<br> The car is a ' . $this->name . 'and its color is ' . $this->color . ".";
+    //     }
+    // };
+
+    // $myCar = new Car('Toyota', 'Red');
+    // echo $myCar->getInfo();
+
+    // $date = date('H');
+
+    // if(5 > 6){
+    //     echo "Hello World";
+    // }else{
+    //     echo "How Are you!";
+    //     echo($date);
+    // };
+?>
+
+<?php 
+    // $number = 10;
+    // if($number > 10){
+    //     echo 'The number is greater than 10. <br>';
+    // }elseif($number == 10){
+    //     echo 'The number is equal to 10. <br>';
+    // }else{
+    //     echo 'The number is less than 10. <br>';
+    // }
+
+    // $age = 10;
+    // if($age < 13){
+    //     echo 'Child <br>';
+    // }elseif($age <= 20){
+    //     echo 'Teenager <br>';
+    // }elseif($age < 65){
+    //     echo 'Adult <br>';
+    // }else{
+    //     echo 'Senior <br>';
+    // }
+
+    // $score = 50;
+    // if($score >= 90){
+    //     echo 'Grade: A <br>';
+    // }elseif($score >= 80){
+    //     echo "Grade: B <br>";
+    // }elseif($score >= 70){
+    //     echo 'Grade: D <br>';
+    // }else{
+    //     echo 'Grade: F <br>';
+    // }
+
+    // $temperature = 15;
+    // if($temperature > 35){
+    //     echo "It's a hot day. <br>";
+    // }elseif($temperature >20){
+    //     echo "It's a warm day. <br>";
+    // }elseif($temperature > 10 ){
+    //     echo "It's a cool day. <br>";
+    // }else{
+    //     echo "It's a cold day. <br>";
+    // };
+
+    // $hour = 10;
+    // if($hour < 12){
+    //     echo 'Good Morging! <br>';
+    // }elseif($hour < 18){
+    //     echo 'Good afternoon! <br>';
+    // }else{
+    //     echo 'Good evening! <br>';
+    // };
+
+    // $a = 10;
+    // $b = 20;
+    // $operation =  '';
+    // if($operation == 'add <br>'){
+    //     echo $a + $b;
+    // }elseif($operation == 'subtract <br>'){
+    //     echo $a - $b;
+    // }elseif($operation == 'multiply <br>'){
+    //     echo $a * $b;
+    // }elseif($operation == 'divide'){
+    //     echo $b != 0 ? $a / $b : 'Cannot divide by zero. <br>';
+    // }else{
+    //     echo 'Invalid operation. <br>';
+    // }
+
+    // $user_role = 'subscriberss';
+    // if($user_role == 'admin'){
+    //     echo 'Welcome, admin <br>';
+    // }elseif($user_role == 'editor'){
+    //     echo 'Welcome, Editor! <br>';
+    // }elseif($user_role == 'subscriber'){
+    //     echo 'Welcome, Subscriber! <br>';
+    // }else{
+    //     echo 'Role not recogized. <br>';
+    // };
+
+    // $rating = 9;
+    // if($rating >= 8){
+    //     echo 'Highly recommended! <br>';
+    // }elseif($rating >= 5){
+    //     echo 'Worth watching. <br>';
+    // }else{
+    //     echo 'Not recommended! <br>';
+    // };
+
+    // $light = 'black';
+    // if($light == 'red'){
+    //     echo 'Stop! <br>';
+    // }elseif($light == 'yellow'){
+    //     echo 'Get ready to stop. <br>';
+    // }elseif($light == 'green'){
+    //     echo 'Go! <br>';
+    // }else{
+    //     echo 'Invalid traffic light.<br>';
+    // };
+
+    // $x = "hi";
+    // $y = 'hello';
+    // if($x > $y){
+    //     echo '$x is greater than $y. <br>';
+    // }elseif($x < $y){
+    //     echo '$x is less than $y. <br>';
+    // }else{
+    //     echo '$x is equal to $y. <br>';
+    // }
+
+?>
